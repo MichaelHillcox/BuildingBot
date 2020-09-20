ecab45f951134eb3274c6fa2c7bb551bfb16c770
Michael Hillcox <michaelhillcox@gmail.com>
2020-09-20 13:02:15 +0100

    Added config.tmp.js and gitignore the real one

---
+++ config.tmp.js
@@ -0,0 +1,18 @@
+module.exports = {
+  discord: {
+    token: '',
+    channels: {
+      issues: '',
+      pr: ''
+    }
+  },
+  api: {
+    github: 'https://api.github.com/',
+    discord: 'https://discord.com/api/'
+  },
+  github: {
+    owner: 'Direwolf20-MC',
+    repo: 'BuildingGadgets',
+    token: ''
+  }
+}
