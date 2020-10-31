3ee6407b9ff8424ff9a3bb7373a217515d402278
Michael Hillcox <michaelhillcox@gmail.com>
2020-10-31 18:39:55 +0000

    Don't dump other processes into the workflow output

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-pm2 stop buildingbot
+cd /home/bots/node/buildingbot/
+pm2 stop buildingbot > /dev/null 2>&1
 rm -rf build
 git pull origin master
 npm ci
 npm run build
-pm2 start buildingbot
+pm2 start buildingbot > /dev/null 2>&1
+pm2 status | grep buildingbot
